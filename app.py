88105be7395dc0f3e8ec160c331b389acf52555a
AmarnadhReddy055 <134163887+AmarnadhReddy055@users.noreply.github.com>
2024-05-16 18:41:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,40 @@
+from huggingface_hub import login
+from transformers import AutoTokenizer, pipeline
+import torch
 import streamlit as st
-from huggingface_hub import hf_hub_download
-from llama_cpp import Llama
 
-# Define model details
-model_name_or_path = "TheBloke/Llama-2-13B-chat-GGML"
-model_basename = "llama-2-13b-chat.ggmlv3.q5_1.bin"
+login(token='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')
 
-# Download the model file from Hugging Face Hub
-model_path = hf_hub_download(repo_id=model_name_or_path, filename=model_basename)
+model = "meta-llama/Llama-2-7b-chat-hf" 
 
-# Initialize the model
-lcpp_llm = Llama(
-    model_path=model_path,
-    n_threads=2,
-    n_batch=2,
-    n_gpu_layers=32
+
+tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
+
+
+llama_pipeline = pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
 )
 
-# Streamlit app interface
-st.title("Llama-2 Chatbot")
-st.write("Enter your prompt below and receive a response from the Llama-2 model.")
 
-# Input prompt
-prompt = st.text_area("Your prompt:")
+st.title("Llama-2-7b Chatbot")
+st.write("Enter a prompt and get a response from the Llama-2-7b model.")
 
-if st.button("Generate Response"):
-    # Define the prompt template
-    prompt_template = f'''SYSTEM: You are a helpful, respectful, and honest assistant. Always answer as helpfully as possible.
+# Text input for the user's prompt
+user_prompt = st.text_input("Enter your prompt here:")
 
-    USER: {prompt}
-
-    ASSISTANT:
-    '''
-    
-    # Generate the response
-    response = lcpp_llm(
-        prompt=prompt_template, 
-        max_tokens=256, 
-        temperature=0.5, 
-        top_p=0.95,
-        repeat_penalty=1.2, 
-        top_k=150
+if user_prompt:
+    # Generate sequences using the Llama-2-7b model
+    sequences = llama_pipeline(
+        user_prompt,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=256,
     )
 
-    # Extract and display the assistant's response
-    assistant_response = response["choices"][0]["text"].split("ASSISTANT:")[1].strip()
-    st.write("## Response:")
-    st.write(assistant_response)
+    # Display the generated text
+    st.write("Chatbot:", sequences[0]['generated_text'])
